1bffdf9fbcc27cf909b44f443132095e71316cae
bayac <baya.chaymae123@gmail.com>
2025-11-01 12:24:46 +0100

    g

---
+++ week1/Day1/ExcerciseXP/Your_computer_brand.py
@@ -0,0 +1,2 @@
+computer_brand = " Hp "
+print("I have a" + computer_brand + "computer.")
---
+++ week1/Day1/ExcerciseXP/Your_information.py
@@ -0,0 +1,6 @@
+name = "chaymae"
+age = 25
+shoe_size = 37
+info = " I'm " + name + " and I have " + str(age)  + " years old , " +  "and I wear size" +  str(shoe_size) + " shoes " +  "I love learning new technologies and solving challenging problems "
+print(info)
+
